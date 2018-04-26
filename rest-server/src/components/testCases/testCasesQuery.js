c932233e1c5958e73d136ff1956ae18102a9818e
Nikko Sanchez <neeks08@gmail.com>
2018-04-25 17:25:13 -0700

    Travie

--- rest-server/src/components/testCases/testCasesQuery.js
+++ rest-server/src/components/testCases/testCasesQuery.js
@@ -5,8 +5,9 @@ import {
 } from './testCasesSQLHelpers';
 
 export const testCaseQuery = async (payload, url) => {
+  console.log('here payload: ',payload)
   if (url === '/') {
-    return await globalQueryHelper(payload, addTestCaseHelper, 'addTestCaseHelper', ['content', 'challenge_id']);
+    return await globalQueryHelper(payload, addTestCaseHelper, 'addTestCaseHelper', ['testcode', 'challenge_id']);
   } else {
     return await globalQueryHelper(payload, fetchAllTestCasesHelper, 'fetchAllTestCasesHelper');
   }
