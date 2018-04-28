1e19f4dc6fb69730bd2e8bc98910677002ef36a7
Daniel Lai <dlai813@gmail.com>
2018-04-27 17:32:35 -0700

    moved scripts

--- envSetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-yarn &
-echo "yarn" &
-yarn buildEnv &
-echo "yarn buildEnv" &
-yarn setup &
-echo "yarn setup"
---
+++ scripts/envSetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "I AM HERE"
+# yarn &
+# echo "yarn" &
+# yarn buildEnv &
+# echo "yarn buildEnv" &
+# yarn setup &
+# echo "yarn setup"
